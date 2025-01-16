d16c746b6a5ac0cc23c9bed096bd0e0a2d9d0f7f
HugoHMZ <hugo.humez.p@gmail.com>
2025-01-17 00:46:15 +0900

    [*] deck discard: component refacto for bette readability

--- src/app/_components/Gameboard/_subcomponents/PlayerTray/DeckDiscard.tsx
+++ src/app/_components/Gameboard/_subcomponents/PlayerTray/DeckDiscard.tsx
@@ -8,101 +8,99 @@ import { usePopup } from '@/app/_contexts/Popup.context';
 const DeckDiscard: React.FC<IDeckDiscardProps> = (
     trayPlayer
 ) => {
-    const { gameState } = useGame();
+    const { gameState, connectedPlayer } = useGame();
     const { togglePopup } = usePopup();
 
-    // ------------------------STYLES------------------------//
-    const containerStyle = {
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '1vw',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
-
-    // ------------------------DISCARD------------------------//
-    const selectableStyle = {
-        '&:hover': {
-            backgroundColor: '#282828',
-            cursor: 'pointer',
-            scale: '1.1',
-            transition: 'all ease-in-out 0.15s',
+    const styles = {
+        containerStyle: {
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '1vw',
+            justifyContent: 'center',
+            alignItems: 'center',
         },
+        discard: {
+            discardCardStyle: {
+                backgroundColor: '#282828E6',
+                width: '7vh',
+                height: '9.5vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+
+                '&:hover': {
+                    backgroundColor: '#282828',
+                    cursor: 'pointer',
+                    scale: '1.1',
+                    transition: 'all ease-in-out 0.15s',
+                },
+            },
+            discardContentStyle: {
+                fontFamily: 'var(--font-barlow), sans-serif',
+                fontWeight: '800',
+                fontSize: '1.2em',
+                color: 'white',
+                textAlign: 'center',
+            }
+        },
+        deck: {
+
+            deckCardStyle: {
+                backgroundColor: 'black',
+                backgroundPosition: 'center',
+                backgroundSize: 'contain',
+                backgroundImage: 'url(\'/card-back.png\')',
+                backgroundRepeat: 'no-repeat',
+
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+
+                width: '7vh',
+                height: '9.5vh',
+            },
+
+            deckContentStyle: {
+                fontFamily: 'var(--font-barlow), sans-serif',
+                fontWeight: '800',
+                fontSize: '2em',
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: '50px',
+                // Text shadowBox
+                backgroundColor: 'rgba(0, 0, 0, .75)',
+                borderRadius: '100px',
+                width: '50px',
+                height: '50px',
+            }
+        }
     }
 
-    const discardCardStyle = {
-        backgroundColor: '#282828E6',
-        width: '7vh',
-        height: '9.5vh',
-        display: 'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        ...selectableStyle
-    };
-    
-    const discardContentStyle = {
-        fontFamily: 'var(--font-barlow), sans-serif',
-        fontWeight: '800',
-        fontSize: '1.2em',
-        color: 'white',
-        textAlign:'center',
-    };
+    const discardComponent = gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'].length > 0 ?
+        <GameCard card={gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'][0]} /> : <Typography sx={styles.discard.discardContentStyle}>Discard</Typography>
 
-
-    // ------------------------DECK------------------------//
-    const textShadowBoxStyle = {
-        backgroundColor: 'rgba(0, 0, 0, .75)',
-        borderRadius: '100px',
-        width: '50px',
-        height: '50px',
-    };
-
-    const deckCardStyle = {
-        backgroundColor: 'black',
-        backgroundPosition: 'center',
-        backgroundSize:'contain',
-        backgroundImage: 'url(\'/card-back.png\')',
-        backgroundRepeat: 'no-repeat',
-
-        display: 'flex',
-        alignItems:'center',
-        justifyContent:'center',
-
-        width: '7vh',
-        height: '9.5vh',
-    };
-    
-    const deckContentStyle = {
-        fontFamily: 'var(--font-barlow), sans-serif',
-        fontWeight: '800',
-        fontSize: '2em',
-        color: 'white',
-        textAlign:'center',
-        lineHeight: '50px',
-        ...textShadowBoxStyle
-    };
+    const deckComponent = <Typography sx={styles.deck.deckContentStyle}>{gameState?.players[trayPlayer.trayPlayer]?.cardPiles['deck'].length}</Typography>
 
     return (
-        <Box sx={containerStyle}>
+        <Box sx={styles.containerStyle}>
             <Card
-                sx={discardCardStyle}
-                onClick={() =>
+                sx={styles.discard.discardCardStyle}
+                onClick={() => {
+                    const playerName = connectedPlayer != trayPlayer.trayPlayer ? "Your Opponent\'s" : "Your";
+
                     togglePopup('pile', {
                         uuid: `${trayPlayer.trayPlayer}-discard`,
-                        title: `${trayPlayer.trayPlayer}'s discard`,
+                        title: `${playerName} discard`,
                         cards:
-                        gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'],
+                            gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'],
                     })
                 }
-            >
-                {
-                    gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'].length > 0 ?
-                        <GameCard card={gameState?.players[trayPlayer.trayPlayer]?.cardPiles['discard'][0]} /> : <Typography sx={discardContentStyle}>Discard</Typography>
                 }
+            >
+                {discardComponent}
             </Card>
-            <Card sx={deckCardStyle}>
-                <Typography sx={deckContentStyle}>{gameState?.players[trayPlayer.trayPlayer]?.cardPiles['deck'].length}</Typography>
-
+            <Card sx={styles.deck.deckCardStyle}>
+                {deckComponent}
             </Card>
         </Box>
     );
