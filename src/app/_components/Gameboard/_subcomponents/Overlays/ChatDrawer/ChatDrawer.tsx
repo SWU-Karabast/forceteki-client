627a7ebe6d6dc9cb0f0746e3aca96e37ca3be1e3
Dan Bastin <dan.bastin@gmail.com>
2025-02-21 22:23:33 -0500

    cleanup styles

--- src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
+++ src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
@@ -1,68 +1,60 @@
-import React from 'react';
-import { Drawer, Box, IconButton, Typography } from '@mui/material';
-import { Settings, Close } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { Drawer, Box, Typography } from '@mui/material';
 import Chat from '@/app/_components/_sharedcomponents/Chat/Chat';
 import { IChatDrawerProps } from '@/app/_components/Gameboard/GameboardTypes';
 import { useGame } from '@/app/_contexts/Game.context';
 
 const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen }) => {
-    const { gameState } = useGame();
+    const { gameState, sendGameMessage } = useGame();
+    const [chatMessage, setChatMessage] = useState('')
+
+    const handleGameChat = () => {
+        sendGameMessage(['chat', chatMessage]);
+        setChatMessage('');
+    }
 
     // ------------------------STYLES------------------------//
-
-    const drawerStyle = {
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-            backgroundColor: '#000000CC',
-            color: '#fff',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            width: '280px',
+    const styles = {
+        drawerStyle: {
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                backgroundColor: '#000000CC',
+                color: '#fff',
+                display: 'flex',
+                flexDirection: 'column',
+                width: '280px',
+                padding: '1em'
+            },
         },
-    };
-    const headerBoxStyle = {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    };
+        headerBoxStyle: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+        }
+    }
 
-    const imagePlaceholderBoxStyle = {
-        backgroundColor: '#333',
-        height: '150px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    };
+    
 
     return (
         <Drawer
             anchor="right"
             open={sidebarOpen}
             variant="persistent"
-            sx={drawerStyle}
+            sx={styles.drawerStyle}
         >
-            <Box sx={{ p: 2 }}>
-                <Box sx={headerBoxStyle}>
-                    <Typography variant="h3" sx={{ m: 0 }}>
-                        ROUND
-                    </Typography>
-                </Box>
-
-                <Box sx={{ mt: 1 }}>
-                    <Typography style={{ fontWeight: 'bold' }}>Last Played</Typography>
-                    <Box sx={imagePlaceholderBoxStyle}>
-                        <Typography style={{ color: '#888' }}>
-                            Image Placeholder
-                        </Typography>
-                    </Box>
-                </Box>
-
-                {/* Use the ChatComponent here */}
-                <Chat
-                    chatHistory={gameState.gameChat}
-                />
+            <Box sx={styles.headerBoxStyle}>
+                <Typography variant="h3">
+                    ROUND
+                </Typography>
             </Box>
+
+            {/* Use the ChatComponent here */}
+            <Chat
+                chatHistory={gameState.gameChat}
+                chatMessage={chatMessage}
+                setChatMessage={setChatMessage}
+                handleChatSubmit={handleGameChat}
+            />
         </Drawer>
     );
 };
--- src/app/_components/_sharedcomponents/Chat/Chat.tsx
+++ src/app/_components/_sharedcomponents/Chat/Chat.tsx
@@ -18,69 +18,68 @@ const Chat: React.FC<IChatProps> = ({
 }) => {
     // ------------------------STYLES------------------------//
 
-    const chatContainerStyle = {
-        backgroundColor: '#28282800',
-        height: '55vh',
-        overflowY: 'auto',
-    };
-
-    const titleStyle = {
-        fontWeight: 'bold',
-        color: '#fff',
-        fontSize: '1.5em',
-    };
-
-    const dividerStyle = {
-        backgroundColor: '#fff',
-        mt: '.5vh',
-        mb: '0.5vh',
-    };
-
-    const chatBoxStyle = {
-        p: '10px',
-        borderRadius: '4px',
-        minHeight: '100px',
-        overflowY: 'auto',
-        backgroundColor: '#28282800',
-    };
-
-    const messageTextStyle = {
-        color: '#fff',
-    };
-
-    const inputContainerStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: '#28282800',
-        p: '10px',
-        mt: 2,
-    };
-    const textFieldStyle = {
-        backgroundColor: '#28282800',
-        color: '#fff',
-        borderRadius: '4px',
-        flexGrow: 1,
-        input: { color: '#fff' },
-        '& .MuiOutlinedInput-root': {
-            // base border style
-            '& fieldset': {
-                borderColor: '#fff',
-            },
+    const styles = {
+        chatContainer: {
+            backgroundColor: '#28282800',
+            overflowY: 'auto',
+            height: '100%'
         },
-        '& .MuiOutlinedInput-root.Mui-focused': {
-            // when container is focused
-            '& fieldset': {
-                borderColor: '#fff',
-            },
+        title: {
+            fontWeight: 'bold',
+            color: '#fff',
+            fontSize: '1.5em',
         },
-    };
+        divider: {
+            backgroundColor: '#fff',
+            mt: '.5vh',
+            mb: '0.5vh',
+        },
+        chatBox: {
+            p: '10px',
+            borderRadius: '4px',
+            minHeight: '100px',
+            overflowY: 'auto',
+            backgroundColor: '#28282800',
+        },
+        messageText: {
+            color: '#fff',
+        },
+        inputContainer: {
+            display: 'flex',
+            alignItems: 'center',
+            backgroundColor: '#28282800',
+            p: '10px',
+            mt: 2,
+        },
+        textField: {
+            backgroundColor: '#28282800',
+            color: '#fff',
+            borderRadius: '4px',
+            flexGrow: 1,
+            input: { color: '#fff' },
+            '& .MuiOutlinedInput-root': {
+                // base border style
+                '& fieldset': {
+                    borderColor: '#fff',
+                },
+            },
+            '& .MuiOutlinedInput-root.Mui-focused': {
+                // when container is focused
+                '& fieldset': {
+                    borderColor: '#fff',
+                },
+            },
+        }
+    }
+
+    
 
     return (
         <>
-            <Box sx={chatContainerStyle}>
-                <Typography sx={titleStyle}>Chat</Typography>
-                <Divider sx={dividerStyle} />
-                <Box sx={chatBoxStyle}>
+            <Box sx={styles.chatContainer}>
+                <Typography sx={styles.title}>Chat</Typography>
+                <Divider sx={styles.divider} />
+                <Box sx={styles.chatBox}>
                     {/* {chatHistory.messages && chatHistory.messages.map((chatEntry, index) => {
                         // [ { name: 'Order66', email: null }, 'test', 'some message text' ]
                         // Extract the sender (an object with name) and the text
@@ -94,7 +93,7 @@ const Chat: React.FC<IChatProps> = ({
                 </Box>
             </Box>
 
-            <Box sx={inputContainerStyle}>
+            <Box sx={styles.inputContainer}>
                 <TextField
                     variant="outlined"
                     placeholder="Chat"
@@ -107,7 +106,7 @@ const Chat: React.FC<IChatProps> = ({
                             handleChatSubmit();
                         }
                     }}
-                    sx={textFieldStyle}
+                    sx={styles.textField}
                     slotProps={{
                         input: {
                             endAdornment: (
