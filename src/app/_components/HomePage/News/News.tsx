e87ff10ba5927daada316a3469c234cc5690c970
Dan Bastin <dan.bastin@gmail.com>
2025-03-09 12:23:24 -0400

    update news section (#204)

--- src/app/_components/HomePage/News/News.tsx
+++ src/app/_components/HomePage/News/News.tsx
@@ -26,9 +26,9 @@ const NewsColumn: React.FC = () => {;
 
             <Card variant="blue" sx={styles.notice}>
                 <CardContent>
-                    <Typography variant="bodyBold">Karabast is an open-source, fan-made platform.</Typography>
-                    <Typography variant="body1">It is an educational tool only, meant to facilitate researching decks and strategies that is supportive of in-person play. As such, direct competition through the form of automated tournaments or rankings will not be added.</Typography>
-                    <Typography variant="body1">This tool is free to use and is published non-commercially. Payment is not required to access any functionality.</Typography>
+                    <Typography variant="bodyBold">Pardon our dust!</Typography>
+                    <Typography variant="body1">We are working on bringing you important platform features. This will often result in a brief mainentnence downtime which we are doing in the mornings EST. These will become less frequent as we get closer to being feature complete.</Typography>
+                    <Typography variant="body1">We appreciate your patience as we make Karabast the best it can be!</Typography>
                 </CardContent>
             </Card>
 
