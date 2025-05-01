101d2963fc2fed9f938d5919e7fadaeadd4127ff
Addison Mayberry <admayber@microsoft.com>
2025-05-01 17:35:02 -0400

    Disable linting at build time

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['karabast-data.s3.amazonaws.com'],
     },
+    eslint: {
+        // TODO: re-enable linting if we can get vscode to do auto-fixes like with the BE repo setup
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
