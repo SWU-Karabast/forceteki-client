cd12c1150f6542b647f7c6b522e2c35b4360bff5
AMMayberry1 <ammayberry1@gmail.com>
2025-05-01 23:06:00 -0400

    Re-enable Next Set Preview and some cleanup

--- src/app/_components/Lobby/_subcomponents/SetUpCard/SetUpCard.tsx
+++ src/app/_components/Lobby/_subcomponents/SetUpCard/SetUpCard.tsx
@@ -402,22 +402,17 @@ const SetUpCard: React.FC<ISetUpProps> = ({
                                 <Link href="https://www.swustats.net/" target="_blank" sx={{ color: 'lightblue' }}>
                                     SWU Stats
                                 </Link>{' '}
-                                or{' '}
+                                /{' '}
                                 <Link href="https://www.swudb.com/" target="_blank" sx={{ color: 'lightblue' }}>
                                     SWUDB
                                 </Link>{' '}
-                                or{' '}
-                                {/* <Link
-                                    href="https://www.sw-unlimited-db.com/"
-                                    target="_blank"
-                                    sx={{ color: 'lightblue' }}
-                                >
+                                /{' '}
+                                <Link href="https://sw-unlimited-db.com/" target="_blank" sx={{ color: 'lightblue' }}>
                                     SW-Unlimited-DB
-                                </Link>{' '}*/}
+                                </Link>{' '}
                                 Deck Link{' '}
-                                <Typography variant="body1" sx={styles.labelTextStyleSecondary}>
-                                    (use the URL or &apos;Deck Link&apos; button)
-                                </Typography>
+                                <br />
+                                OR paste deck JSON directly
                             </Box>
                             <StyledTextField
                                 type="text"
