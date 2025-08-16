063b1330ec4a85d4d69348c6cbb17f3d4c9a2474
Matevž Ogrinc <matevz.ogrinc@ijs.si>
2025-08-16 20:41:50 +0200

    Chebato/undo fe (#439)

--- src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
+++ src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
@@ -4,16 +4,27 @@ import Chat from '@/app/_components/_sharedcomponents/Chat/Chat';
 import { IChatDrawerProps } from '@/app/_components/Gameboard/GameboardTypes';
 import { useGame } from '@/app/_contexts/Game.context';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import PreferenceButton from '@/app/_components/_sharedcomponents/Preferences/_subComponents/PreferenceButton';
+import Image from 'next/image';
 
 const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen, toggleSidebar }) => {
-    const { gameState, sendGameMessage } = useGame();
+    const { gameState, sendGameMessage, connectedPlayer } = useGame();
     const [chatMessage, setChatMessage] = useState('')
-
+    const [isUndoHovered, setIsUndoHovered] = useState(false);
+    const isDev = process.env.NODE_ENV === 'development';
+    const correctPlayer = gameState.players[connectedPlayer];
     const handleGameChat = () => {
         const trimmed = chatMessage.trim();
         if (!trimmed) return; // don't send empty messages
         sendGameMessage(['chat', trimmed]);
         setChatMessage('');
+    }
+    const handleUndo = () => {
+        sendGameMessage(['rollbackToSnapshot',{
+            type: 'quick',
+            playerId: connectedPlayer,
+            actionOffset: 0
+        }])
     }
 
     // ------------------------STYLES------------------------//
@@ -35,6 +46,14 @@ const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen, toggleSidebar }) 
             alignItems: 'center',
             position: 'relative',
             height: '2.5em',
+        },
+        actionUndo:{
+            height:'50px',
+            mr:'30px'
+        },
+        actionUndoBox:{
+            display: 'flex',
+            justifyContent:'center',
         }
     }
 
@@ -58,6 +77,25 @@ const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen, toggleSidebar }) 
                     width: 'max-content',
                 }}>Chat</Typography>
             </Box>
+            {isDev && (<Box sx={styles.actionUndoBox}>
+                <Image
+                    src="/porg1.png"
+                    alt="Highlighted Stats Panel"
+                    width={50}
+                    height={50}
+                    style={{
+                        position:'relative',
+                        left:'35px',
+                        bottom:'28px',
+                        visibility: isUndoHovered ? 'visible' : 'hidden',
+                    }}
+                />
+                <PreferenceButton sx={styles.actionUndo} buttonFnc={handleUndo} variant={'standard'} text={'Action Undo'}
+                    onMouseEnter={() => setIsUndoHovered(true)}
+                    onMouseLeave={() => setIsUndoHovered(false)}
+                    disabled={!correctPlayer['availableSnapshots']?.hasQuickSnapshot}
+                />
+            </Box>)}
 
             {/* Use the ChatComponent here */}
             <Chat
--- src/app/_components/_sharedcomponents/Preferences/PreferencesSubElementVariants/CurrentGameTab.tsx
+++ src/app/_components/_sharedcomponents/Preferences/PreferencesSubElementVariants/CurrentGameTab.tsx
@@ -13,11 +13,12 @@ import BugReportDialog from '@/app/_components/_sharedcomponents/Preferences/_su
 
 function CurrentGameTab() {
     const { sendGameMessage, connectedPlayer, gameState, isSpectator } = useGame();
-
+    const isDev = process.env.NODE_ENV === 'development';
     const router = useRouter();
     const currentPlayerName = gameState.players[connectedPlayer]?.name;
     const [confirmConcede, setConfirmConcede] = useState<boolean>(false);
     const [bugReportOpen, setBugReportOpen] = useState<boolean>(false);
+
 
     useEffect(() => {
         if(confirmConcede){
@@ -37,6 +38,24 @@ function CurrentGameTab() {
             setConfirmConcede(false);
         }
     };
+
+    // Click handler for the undo button
+    const handleUndo = () => {
+        sendGameMessage(['rollbackToSnapshot',{
+            type: 'action',
+            playerId: connectedPlayer,
+            actionOffset: 0
+        }])
+    }
+
+    // Click handler for the undo button
+    const handleUndoPhase = () => {
+        sendGameMessage(['rollbackToSnapshot',{
+            type: 'phase',
+            phaseName: gameState.phase,
+            actionOffset: 0
+        }])
+    }
 
     // Handler for opening the bug report dialog
     const handleOpenBugReport = () => {
@@ -102,22 +121,24 @@ function CurrentGameTab() {
                     </Box>
                 </Box>
             )}
-            {/* <Box sx={styles.functionContainer}>
-                <Typography sx={styles.typographyContainer} variant={'h3'}>Undo</Typography>
-                <Divider sx={{ mb: '20px' }}/>
-                <Box sx={{ ...styles.contentContainer, mb:'20px' }}>
-                    <PreferenceButton variant={'standard'} text={'Simple Undo'} />
-                    <Typography sx={styles.typeographyStyle}>
-                        Revert to your previous game state.
-                    </Typography>
+            {isDev && (
+                <Box sx={styles.functionContainer}>
+                    <Typography sx={styles.typographyContainer} variant={'h3'}>Undo</Typography>
+                    <Divider sx={{ mb: '20px' }}/>
+                    <Box sx={{ ...styles.contentContainer, mb:'20px' }}>
+                        <PreferenceButton variant={'standard'} text={'Action Undo'} buttonFnc={handleUndo}/>
+                        <Typography sx={styles.typeographyStyle}>
+                            Revert to your previous game state.
+                        </Typography>
+                    </Box>
+                    <Box sx={styles.contentContainer}>
+                        <PreferenceButton variant={'standard'} text={'Phase Undo'} buttonFnc={handleUndoPhase}/>
+                        <Typography sx={styles.typeographyStyle}>
+                            Revert to the start of this phase.
+                        </Typography>
+                    </Box>
                 </Box>
-                <Box sx={styles.contentContainer}>
-                    <PreferenceButton variant={'standard'} text={'This Turn'} />
-                    <Typography sx={styles.typeographyStyle}>
-                        Revert to the start of the previous turn.
-                    </Typography>
-                </Box>
-            </Box> */}
+            )}
             <Box sx={{ ...styles.functionContainer, mb:'0px' }}>
                 <Typography sx={styles.typographyContainer} variant={'h3'}>Thanks for playing</Typography>
                 <Divider sx={{ mb: '20px' }}/>
