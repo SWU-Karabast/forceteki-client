3a2a152f746829d5c35668c422408da7b580ab83
phever <3782319+phever@users.noreply.github.com>
2025-09-01 02:37:25 -0600

    change home page public lobbies to spectate to reverse sorting order - this feels more natural. added a dropdown and leader filtering as well

--- src/app/_components/HomePage/_subcomponents/GamesInProgress/GamesInProgress.tsx
+++ src/app/_components/HomePage/_subcomponents/GamesInProgress/GamesInProgress.tsx
@@ -5,7 +5,7 @@ import { ICardData } from '@/app/_components/_sharedcomponents/Cards/CardTypes';
 
 interface GameCardData {
     id: string;
-    isPrivate:boolean;
+    isPrivate: boolean;
     player1Leader: ICardData;
     player1Base: ICardData;
     player2Leader: ICardData;
@@ -17,7 +17,8 @@ interface OngoingGamesData {
     ongoingGames: GameCardData[];
 }
 
-const fetchOngoingGames = async (setGamesData: (games: OngoingGamesData | null) => void) => {
+const fetchOngoingGames = async (setGamesData: (games: OngoingGamesData | null) => void,
+    sortByRecent: 'asc' | 'desc') => {
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_URL}/api/ongoing-games`);
 
@@ -25,8 +26,11 @@ const fetchOngoingGames = async (setGamesData: (games: OngoingGamesData | null) 
             throw new Error(`Failed to fetch ongoing games: ${response.status} ${response.statusText}`);
         }
 
-        const fetchedData: OngoingGamesData = await response.json(); 
+        const fetchedData: OngoingGamesData = await response.json();
         fetchedData.ongoingGames.sort((a, b) => Number(a.isPrivate) - Number(b.isPrivate));
+        if (sortByRecent === 'desc') {
+            fetchedData.ongoingGames.reverse();
+        }
         setGamesData(fetchedData);
     } catch (err) {
         console.error('Error fetching ongoing games:', err);
@@ -37,19 +41,21 @@ const fetchOngoingGames = async (setGamesData: (games: OngoingGamesData | null) 
 
 const GamesInProgress: React.FC = () => {
     const [gamesData, setGamesData] = useState<OngoingGamesData | null>(null);
+    const [sortByRecent, setSortByRecent] = useState<'asc' | 'desc'>('asc');
+    const [sortByLeader, setSortByLeader] = useState<string>('');
 
     useEffect(() => {
         let count = 0;
         let intervalId: NodeJS.Timeout;
 
         const fetchData = () => {
-            fetchOngoingGames(setGamesData);
+            fetchOngoingGames(setGamesData, sortByRecent);
             count++;
 
             if (count === 6) {
                 clearInterval(intervalId);
                 intervalId = setInterval(() => {
-                    fetchOngoingGames(setGamesData);
+                    fetchOngoingGames(setGamesData, sortByRecent);
                     count++;
                     if (count === 15) {
                         clearInterval(intervalId);
@@ -62,7 +68,7 @@ const GamesInProgress: React.FC = () => {
         intervalId = setInterval(fetchData, 10000); // Fetch every 10 seconds
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [sortByRecent]);
 
     const styles = {
         headerBox: {
@@ -87,9 +93,30 @@ const GamesInProgress: React.FC = () => {
                 <Typography variant="h3">Games in Progress</Typography>
                 <Typography variant="h3" sx={styles.activeGamesNumber}>{gamesData?.numberOfOngoingGames || 0}</Typography>
             </Box>
+            <div style={{
+                display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingTop: '5px'
+            }}>
+                <Typography variant="body2">Sort By:</Typography>
+                <select
+                    value={sortByRecent}
+                    onChange={e => setSortByRecent(e.target.value as 'asc' | 'desc')}
+                >
+                    <option value="asc">Oldest First</option>
+                    <option value="desc">Newest First</option>
+                </select>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="body2">Filter by Leader:</Typography>
+                <input
+                    value={sortByLeader}
+                    onChange={e => setSortByLeader(e.target.value)}
+                    style={{ maxWidth: '40%' }}
+                ></input>
+            </div>
             <Divider sx={styles.divider} />
             <Box>
-                {gamesData?.ongoingGames.map((match, index) => (
+                {gamesData?.ongoingGames.filter((match) => match.player1Leader.name?.includes(sortByLeader) || match.player2Leader
+                    .name?.includes(sortByLeader)).map((match, index) => (
                     <PublicMatch key={index} match={match} />
                 ))}
             </Box>
