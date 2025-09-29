9a5569ef080a4689e34fdcde77f7387be38275c5
Addison Mayberry <admayber@microsoft.com>
2025-09-29 18:16:39 -0400

    WIP

--- src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
+++ src/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/ChatDrawer.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Drawer, Box } from '@mui/material';
+import { Drawer, Box, Button } from '@mui/material';
 import Chat from '@/app/_components/_sharedcomponents/Chat/Chat';
 import { IChatDrawerProps } from '@/app/_components/Gameboard/GameboardTypes';
 import { useGame } from '@/app/_contexts/Game.context';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import UndoIcon from '@mui/icons-material/Undo';
-import ChatButtonComponent from '@/app/_components/Gameboard/_subcomponents/Overlays/ChatDrawer/_subComponents/ChatButtonComponent';
 import Image from 'next/image';
 
 const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen, toggleSidebar }) => {
@@ -84,21 +83,55 @@ const ChatDrawer: React.FC<IChatDrawerProps> = ({ sidebarOpen, toggleSidebar }) 
                     height={50}
                     style={{
                         position:'absolute',
-                        right:'10px',
-                        top:'10px',
-                        visibility: isUndoHovered ? 'visible' : 'hidden',
+                        left:'50px',
+                        top:'20px',
+                        // visibility: isUndoHovered ? 'visible' : 'hidden',
                     }}
                 />
-                <ChatButtonComponent 
-                    sx={styles.quickUndo} 
-                    buttonFnc={handleUndoButton} 
-                    variant={'standard'} 
-                    text={'Undo'}
-                    startIcon={<UndoIcon />}
+                <Button
+                    variant="contained"
+                    onClick={handleUndoButton}
                     onMouseEnter={() => setIsUndoHovered(true)}
                     onMouseLeave={() => setIsUndoHovered(false)}
                     disabled={!correctPlayer['availableSnapshots']?.hasQuickSnapshot}
-                />
+                    startIcon={<UndoIcon />}
+                    sx={{
+                        ...styles.quickUndo,
+                        background: 'linear-gradient(rgb(29, 29, 29), #0a3d1e) padding-box, linear-gradient(to top, #1cb34a, #0a3d1e) border-box',
+                        color: '#FFF',
+                        fontSize: '20px',
+                        border: '1px solid transparent',
+                        borderRadius: '10px',
+                        pt: '10px',
+                        pb: '10px',
+                        justifyContent: 'flex-end',
+                        paddingRight: '35px',
+                        position: 'relative',
+                        '& .MuiButton-startIcon': {
+                            position: 'absolute',
+                            left: '12px',
+                            marginRight: 0,
+                            marginLeft: 0,
+                            transform: 'skewX(5deg)',
+                            '& svg': {
+                                width: '23px',
+                                height: '23px',
+                            },
+                        },
+                        '&:hover': {
+                            background: 'linear-gradient(rgb(29, 29, 29),rgb(20, 81, 40)) padding-box, linear-gradient(to top, #2ad44c, #0a3d1e) border-box',
+                            boxShadow: '0 0 8px rgba(0, 170, 70, 0.7)',
+                            border: '1px solid rgba(0, 200, 90, 0.7)',
+                        },
+                        '&:disabled': {
+                            backgroundColor: '#404040',
+                            color: '#FFF'
+                        },
+                        transform: 'skewX(-5deg)',
+                    }}
+                >
+                    Undo
+                </Button>
             </Box>)}
 
             {/* Use the ChatComponent here */}
