11eb8ed3bc2f27de8e5623ff18b418080ee7ab0c
Mario Torres <mariotorresdev@gmail.com>
2025-10-24 18:41:57 -0500

    More Updates

--- src/app/_utils/fetchCosmeticsData.ts
+++ src/app/_utils/fetchCosmeticsData.ts
@@ -10,8 +10,8 @@ export const fetchCosmeticsDataAsync = async (): Promise<Cosmetics> => {
             { id: 'guid1', type: 'cardback', title: 'Default', path: s3ImageURL('game/swu-cardback.webp') },
         ],
         backgrounds: [
-            { id: 'guid2', type: 'background', title: 'Default', path: s3ImageURL('ui/default-background.png') },
-            { id: 'guid3', type: 'background', title: 'Spark of Rebellion Starfield', path: s3ImageURL(`${backgroundPrefix}/SWH01_Starfield.jpg`), darkened: true },
+            { id: 'guid2', type: 'background', title: 'Default', path: '/default-background.webp' },
+            { id: 'guid3', type: 'background', title: 'Spark of Rebellion Starfield', path: s3ImageURL(`${backgroundPrefix}/SWH01_Starfield.png`), darkened: true },
             { id: 'guid4', type: 'background', title: 'Shadows of the Galaxy Starfield', path: s3ImageURL(`${backgroundPrefix}/SWH02_Starfield.png`), darkened: true },
             { id: 'guid5', type: 'background', title: 'Twilight of the Republic Starfield', path: s3ImageURL(`${backgroundPrefix}/SWH03_Starfield.png`), darkened: true },
             { id: 'guid6', type: 'background', title: 'Jump to Lightspeed Starfield', path: s3ImageURL(`${backgroundPrefix}/SWH04_Starfield.png`), darkened: true },
