736170efe9a7aeb26f8734936aa7a0b43633d39c
sethhelman <s7helman@gmail.com>
2026-05-18 15:27:42 -0400

    Update page.tsx

--- src/app/Preferences/page.tsx
+++ src/app/Preferences/page.tsx
@@ -49,7 +49,7 @@ const Preferences: React.FC = () => {
             <PreferencesComponent
                 sidebarOpen={false}
                 isPreferenceOpen={true}
-                tabs={user ? ['general','keyboardShortcuts','soundOptions','cosmetics'] : ['general','soundOptions']}
+                tabs={user ? ['general','gameOptions', 'keyboardShortcuts','soundOptions','cosmetics'] : ['general','soundOptions']}
                 variant={'homePage'}
             />
             <Typography variant="body1" sx={styles.disclaimer}>
