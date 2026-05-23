25180c8c76eef4fc1f19fe0871bedcbc4f92a428
Santiago Vázquez <santiagovazq@gmail.com>
2026-05-23 15:27:33 -0300

    Change link and add a new line break

--- src/app/_components/_sharedcomponents/Preferences/_subComponents/BugReportDialog.tsx
+++ src/app/_components/_sharedcomponents/Preferences/_subComponents/BugReportDialog.tsx
@@ -100,7 +100,7 @@ const StyledSuccessAlert = styled(Alert)(() => ({
 
 const DiscordChannelLink: React.FC<{ children: string }> = ({ children }) => (
     <Link
-        href="https://discord.com/channels/1220057752961814568/1345468050568380568"
+        href="https://discord.gg/uCdUM4bjhP"
         target="_blank"
         rel="noopener noreferrer"
         sx={{ color: 'inherit', textDecoration: 'underline' }}
@@ -211,12 +211,12 @@ const BugReportDialog: React.FC<BugReportDialogProps> = ({ open, onClose }) => {
                         </DiscordChannelLink>
                     </StyledInfoAlert>
                 ): success ? (
-                    <StyledSuccessAlert severity="success" sx={{ my: 1 }}>
-                        Bug report submitted successfully. Thank you for helping improve the game!<br/>
-                        If you&#39;re noticing repeated issues, please reach out to us on our <DiscordChannelLink>
-                            discord channel
-                        </DiscordChannelLink>.
-                    </StyledSuccessAlert>
+                  <StyledSuccessAlert severity="success" sx={{my: 1}}>
+                      Bug report submitted successfully. Thank you for helping improve the game!<br/><br/>
+                      If you&#39;re noticing repeated issues, please reach out to us on our <DiscordChannelLink>
+                      discord channel
+                  </DiscordChannelLink>.
+                  </StyledSuccessAlert>
                 ) : connectedUser?.reportedBugs >= 3 ? (
                     <StyledInfoAlert severity="info" sx={{ my: 1 }}>
                         You&#39;ve sent the maximum number of bug reports for this game session.
