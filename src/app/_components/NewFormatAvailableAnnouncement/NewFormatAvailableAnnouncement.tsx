cdbcffffaad1fa4f5a946bd2c836794dd229f0b2
Addison Mayberry <admayber@microsoft.com>
2026-06-03 19:53:15 -0400

    Allow custom front-page message text and switch to Melee.gg announcement

--- src/app/_components/NewFormatAvailableAnnouncement/NewFormatAvailableAnnouncement.tsx
+++ src/app/_components/NewFormatAvailableAnnouncement/NewFormatAvailableAnnouncement.tsx
@@ -1,22 +1,27 @@
-import { CardPool, FormatLabels, LobbyFormatConfigs, QueueFormatConfigs, SwuGameFormat } from '@/app/_constants/constants'
+import { CardPool, FormatLabels, GameAnnouncement, LobbyFormatConfigs, QueueFormatConfigs } from '@/app/_constants/constants'
 import { Typography } from '@mui/material';
 
-interface INewFormatAvailableAnnouncementProps {
-    format: SwuGameFormat;
-    cardPool?: CardPool;
+interface IGameAnnouncementBannerProps {
+    announcement: GameAnnouncement;
 }
 
-const NewFormatAvailableAnnouncement: React.FC<INewFormatAvailableAnnouncementProps> = ({
-    format,
-    cardPool
-}: INewFormatAvailableAnnouncementProps) => {
-    const isAvailableInQueue = QueueFormatConfigs.some((config) => config.format === format);
-    const isAvailableInLobby = LobbyFormatConfigs.some((config) => config.format === format);
-    const isOnlyInLobby = isAvailableInLobby && !isAvailableInQueue;
+const GameAnnouncementBanner: React.FC<IGameAnnouncementBannerProps> = ({
+    announcement,
+}: IGameAnnouncementBannerProps) => {
+    let text: string;
+    if (announcement.kind === 'custom') {
+        text = announcement.message;
+    } else {
+        const { format, cardPool } = announcement;
+        const isAvailableInQueue = QueueFormatConfigs.some((config) => config.format === format);
+        const isAvailableInLobby = LobbyFormatConfigs.some((config) => config.format === format);
+        const isOnlyInLobby = isAvailableInLobby && !isAvailableInQueue;
+        text = `${FormatLabels[format]} format${cardPool === CardPool.NextSet ? ' with Next Set cards' : ''} is now available${isOnlyInLobby ? ' in Lobbies' : ''}!`;
+    }
 
-    return <Typography variant="body1" sx={{ color: '#70fb6e', textAlign: 'center', mb: '1rem' }}>
-        {FormatLabels[format]} format{cardPool === CardPool.NextSet ? ' with Next Set cards' : ''} is now available{isOnlyInLobby ? ' in Lobbies' : ''}!
+    return <Typography variant="body1" sx={{ color: '#00f0ff', textAlign: 'center', mb: '1rem' }}>
+        {text}
     </Typography>
 }
 
-export default NewFormatAvailableAnnouncement;
+export default GameAnnouncementBanner;
