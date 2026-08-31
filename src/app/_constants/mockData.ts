d009d0a077c2459c342bdd8bc67f5ab0103e1aeb
Robbie Moyer <rmoyer08@gmail.com>
2026-08-31 11:21:54 -0400

    Edit message

--- src/app/_constants/mockData.ts
+++ src/app/_constants/mockData.ts
@@ -88,7 +88,7 @@ export const articles: IArticle[] = [
     {
         title: 'Preview the Homeworlds meta with Next Set cards',
         content: `
-        <p>Homeworlds (Set 9) cards are now playable in Premier & Eternal formats with the "Next Set" card pool. Use this mode to explore new strategies and deck combinations ahead of the official release.</p>
+        <p>Homeworlds (Set 9) cards are now playable in Premier & Eternal formats with the "Next Set" card pool.</p>
 
         <p style="margin-bottom:0;">Join our <a target="_blank" href="https://discord.gg/hKRaqHND4v" style="color:lightblue;">Discord</a> for progress updates and announcements! If you have coding experience, we're always looking for new contributors.</p><p></p>
 	  `,
